f6dca8923bd1a8e2173fd41d1aa1ddb9ae229bfb
aquila_zyy <aquila.zyy@gmail.com>
2020-12-10 06:57:52 +0800

    Remade glass_bottle_block model and texture. Added right click function to FrostHolocronBlock.

--- src/main/java/aquila/myfirstmod/blocks/FrostHolocronBlock.java
+++ src/main/java/aquila/myfirstmod/blocks/FrostHolocronBlock.java
@@ -1,11 +1,20 @@
 package aquila.myfirstmod.blocks;
 
+import javax.annotation.Nullable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class FrostHolocronBlock extends Block {
@@ -27,5 +36,24 @@ public class FrostHolocronBlock extends Block {
   protected void fillStateContainer(Builder<Block, BlockState> builder) {
     builder.add(LIT);
     super.fillStateContainer(builder);
+  }
+
+  @Override
+  public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+    state = state.with(LIT, !state.get(LIT));
+    worldIn.setBlockState(pos, state, 2);
+    playSound(player, worldIn, pos, state.get(LIT));
+    return ActionResultType.func_233537_a_(worldIn.isRemote);
+  }
+
+  protected void playSound(
+      @Nullable PlayerEntity player, World worldIn, BlockPos pos, boolean isLit) {
+    worldIn.playSound(
+        null,
+        pos,
+        isLit ? SoundEvents.BLOCK_BEACON_ACTIVATE : SoundEvents.BLOCK_BEACON_DEACTIVATE,
+        SoundCategory.BLOCKS,
+        1F,
+        1f);
   }
 }
